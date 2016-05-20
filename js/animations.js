3e474fccf7b2a5ae93950bbc8ef41a4f189a3f50
Luciano Bohrer <luciano@4all.mobi>
2016-05-20 16:46:40 -0300

    Added animation to smooth the scroll for the navbar items

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,5 @@
 
-$( document ).ready(function() {
+  /* CHART GENERATOR */
     var chart1 = $("#chart-ios").circliful({
         animationStep: 5,
         foregroundBorderWidth: 5,
@@ -18,6 +18,7 @@ $( document ).ready(function() {
         backgroundBorderWidth: 0,
         percent: 75
     });
+
     var chart4 = $("#chart-ux").circliful({
         animationStep: 5,
         foregroundBorderWidth: 5,
@@ -25,24 +26,25 @@ $( document ).ready(function() {
         percent: 50
     });
 
-    window.sr = ScrollReveal();
-    sr.container = document.querySelector('.about');
+    /* SCROLL ANIMATION TO REVEAL ITEMS */
+    window.sr = new ScrollReveal();
     sr.reveal('.img-me', {
       duration: 1000,
       origin: 'left',
-      distance: '100px'
+      distance: '100px',
+      delay:500
     });
 
     sr.reveal('.text-about', {
       duration: 1000,
       origin: 'right',
-      delay: 500,
+      delay: 1000,
       distance: '100px'
     });
 
     sr.reveal('.screen1',{
       duration: 1000,
-      delay: 150
+      delay: 150,
     });
 
     sr.reveal('.screen4',{
@@ -58,27 +60,46 @@ $( document ).ready(function() {
       duration: 1000,
       delay: 10
     });
+
+
     sr.reveal('.chart1',{
       duration: 1000,
-      delay: 300,
+      delay: 400,
       afterReveal : chart1
+
     });
 
     sr.reveal('.chart2',{
       duration: 1000,
-      delay: 500,
-      afterReveal : chart2
+      delay: 700,
+      afterReset : chart2
     });
 
     sr.reveal('.chart3',{
       duration: 1000,
-      delay: 700,
-      afterReveal : chart3
-
+      delay: 1000,
+      afterReset : chart3
     });
     sr.reveal('.chart4',{
       duration: 1000,
-      delay: 900,
-      afterReveal : chart4
+      delay: 1300,
+      afterReset : chart4
     });
-});
+
+/* SMOOTH NAVIGATION SCROLL */
+  jQuery.noConflict();
+
+  jQuery(document).ready(function($) {
+
+    $('[data-anchor]').click(function() {
+
+    	var target = $($(this).data('anchor'));
+
+    	if (target.length) {
+
+    	$('html, body').animate({
+    	scrollTop: target.offset().top
+    	}, 1000);
+    	}
+    });
+  });
