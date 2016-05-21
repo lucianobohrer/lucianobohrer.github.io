43d570c2eb88d9167a6a0ebdcebb3ac90ca1abc5
Luciano <luciano@4all.com>
2016-05-21 00:20:23 -0300

    New animations, change of colors, finish contact and missing real portfolio

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,10 @@
+/*
+  WOW js
+*/
 
-  /* CHART GENERATOR */
+  new WOW().init();
+
+//   /* CHART GENERATOR */
     var chart1 = $("#chart-ios").circliful({
         animationStep: 5,
         foregroundBorderWidth: 5,
@@ -25,77 +30,77 @@
         backgroundBorderWidth: 0,
         percent: 50
     });
-
-    /* SCROLL ANIMATION TO REVEAL ITEMS */
-    window.sr = new ScrollReveal();
-    sr.reveal('.img-me', {
-      duration: 1000,
-      origin: 'left',
-      distance: '100px',
-      viewFactor  : 0.4,
-      delay:500
-    });
-
-    sr.reveal('.text-about', {
-      duration: 1000,
-      origin: 'right',
-      delay: 1000,
-      viewFactor  : 0.4,
-      distance: '100px'
-    });
-
-    sr.reveal('.screen1',{
-      duration: 1000,
-      viewFactor  : 0.5,
-      delay: 150,
-    });
-
-    sr.reveal('.screen4',{
-      duration: 1000,
-      viewFactor  : 0.5,
-      delay: 150
-    });
-
-    sr.reveal('.screen2',{
-      duration: 1000,
-      viewFactor  : 0.5,
-      delay: 10
-    });
-    sr.reveal('.screen3',{
-      duration: 1000,
-      viewFactor  : 0.5,
-      delay: 10
-    });
-
-
-    sr.reveal('.chart1',{
-      duration: 1000,
-      delay: 400,
-      viewFactor  : 0.5,
-      afterReveal : chart1
-
-    });
-
-    sr.reveal('.chart2',{
-      duration: 1000,
-      delay: 700,
-      viewFactor  : 0.5,
-      afterReset : chart2
-    });
-
-    sr.reveal('.chart3',{
-      duration: 1000,
-      delay: 1000,
-      viewFactor  : 0.5,
-      afterReset : chart3
-    });
-    sr.reveal('.chart4',{
-      duration: 1000,
-      viewFactor  : 0.5,
-      delay: 1300,
-      afterReset : chart4
-    });
-
+//
+//     /* SCROLL ANIMATION TO REVEAL ITEMS */
+//     window.sr = new ScrollReveal();
+//     sr.reveal('.img-me', {
+//       duration: 1000,
+//       origin: 'left',
+//       distance: '100px',
+//       viewFactor  : 0.4,
+//       delay:500
+//     });
+//
+//     sr.reveal('.text-about', {
+//       duration: 1000,
+//       origin: 'right',
+//       delay: 1000,
+//       viewFactor  : 0.4,
+//       distance: '100px'
+//     });
+//
+//     sr.reveal('.screen1',{
+//       duration: 1000,
+//       viewFactor  : 0.5,
+//       delay: 150,
+//     });
+//
+//     sr.reveal('.screen4',{
+//       duration: 1000,
+//       viewFactor  : 0.5,
+//       delay: 150
+//     });
+//
+//     sr.reveal('.screen2',{
+//       duration: 1000,
+//       viewFactor  : 0.5,
+//       delay: 10
+//     });
+//     sr.reveal('.screen3',{
+//       duration: 1000,
+//       viewFactor  : 0.5,
+//       delay: 10
+//     });
+//
+//
+//     sr.reveal('.chart1',{
+//       duration: 1000,
+//       delay: 400,
+//       viewFactor  : 0.5,
+//       afterReveal : chart1
+//
+//     });
+//
+//     sr.reveal('.chart2',{
+//       duration: 1000,
+//       delay: 700,
+//       viewFactor  : 0.5,
+//       afterReset : chart2
+//     });
+//
+//     sr.reveal('.chart3',{
+//       duration: 1000,
+//       delay: 1000,
+//       viewFactor  : 0.5,
+//       afterReset : chart3
+//     });
+//     sr.reveal('.chart4',{
+//       duration: 1000,
+//       viewFactor  : 0.5,
+//       delay: 1300,
+//       afterReset : chart4
+//     });
+//
 /* SMOOTH NAVIGATION SCROLL */
   jQuery.noConflict();
 
